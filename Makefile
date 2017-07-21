8352f7305d557ac4620a91b4c24ca53c49e9a93f
Egor Churaev <egor.churaev@intel.com>
2017-07-21 10:36:39 +0300

    Fixed displaying icons for files. Added default icons for default directories. Fixed problem with makefile.

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 RANGER_DIR=$(if $(XDG_CONFIG_HOME),$(XDG_CONFIG_HOME),$(HOME)/.config)/ranger
 PLUGIN_DIR=$(RANGER_DIR)/plugins
+RC_FILE=$(RANGER_DIR)/rc.conf
+DEVICONS_IN_RC="default_linemode devicons"
 
 install:
 	install -d $(PLUGIN_DIR)
 	install -b devicons.py $(RANGER_DIR)/devicons.py
 	install -b devicons_linemode.py $(PLUGIN_DIR)/devicons_linemode.py
-	echo "default_linemode devicons" >> $(RANGER_DIR)/rc.conf
+	grep -q $(DEVICONS_IN_RC) $(RC_FILE) || echo $(DEVICONS_IN_RC) >> $(RC_FILE)
 
 uninstall:
 	$(RM) $(RANGER_DIR)/devicons.py
--- devicons_linemode.py
+++ devicons_linemode.py
@@ -3,10 +3,17 @@ from ranger.core.linemode import LinemodeBase
 from devicons import *
 
 @ranger.api.register_linemode
-class DevIconsLinemode(ranger.core.linemode.LinemodeBase):
+class DevIconsLinemode(LinemodeBase):
   name = "devicons"
 
   uses_metadata = False
+
+  def filetitle(self, file, metadata):
+    return devicon(file) + ' ' + file.relative_path
+
+@ranger.api.register_linemode
+class DevIconsLinemodeFile(LinemodeBase):
+  name = "filename"
 
   def filetitle(self, file, metadata):
     return devicon(file) + ' ' + file.relative_path

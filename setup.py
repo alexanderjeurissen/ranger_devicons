074da919974f712ad57e729208ecc4ab68a460ce
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2018-05-09 23:33:00 +0200

    TEMP_COMMIT: stashed changes on Wed May  9 21:27:20 CEST 2018

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ranger_devicons',
+  packages = ['ranger_devicons'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Plugin for the Ranger file manager, that provides a linemode that enables file glyph/icon rendering',
+  author = 'Alexander Jeurissen',
+  author_email = 'alexander@jeurissen.email',
+  url = 'https://github.com/alexanderjeurissen/ranger_devicons', # use the URL to the github repo
+  download_url = 'https://github.com/alexanderjeurissen/ranger_devicons/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['ranger', 'devicons', 'nerd-fonts', 'file-icons'], # arbitrary keywords
+  classifiers = [],
+)

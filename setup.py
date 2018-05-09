7240d95defcc442d334a1eedc3f1906d3f897935
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2018-05-10 00:03:04 +0200

    TEMP_COMMIT: stashed changes on Wed May  9 21:27:20 CEST 2018

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 from distutils.core import setup
-setup(
-  name = 'ranger_devicons',
-  packages = ['ranger_devicons'], # this must be the same as the name above
-  version = '1.0',
-  description = 'Plugin for the Ranger file manager, that provides a linemode that enables file glyph/icon rendering',
-  author = 'Alexander Jeurissen',
-  author_email = 'alexander@jeurissen.email',
-  url = 'https://github.com/alexanderjeurissen/ranger_devicons', # use the URL to the github repo
-  download_url = 'https://github.com/alexanderjeurissen/ranger_devicons/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['ranger', 'devicons', 'nerd-fonts', 'file-icons'], # arbitrary keywords
-  classifiers = [],
-)
+import os
+
+# pylint: disable=import-error,no-name-in-module,ungrouped-imports
+if os.environ.get('SETUPTOOLS_USE'):
+  from setuptools import setup
+  from setuptools.command.install_lib import install_lib
+else:
+  from distutils.core import setup
+  from distutils.command.install_lib import install_lib
+# pylint: enable=import-error,no-name-in-module,ungrouped-imports
+
+
+def main():
+  setup(
+    name = 'ranger_devicons',
+    packages = ['ranger_devicons'],
+    version = '1.0',
+    description = 'Plugin for the Ranger file manager, that provides a linemode for file glyph/icon rendering',
+    author = 'Alexander Jeurissen',
+    author_email = 'alexander@jeurissen.email',
+    url = 'https://github.com/alexanderjeurissen/ranger_devicons',
+    download_url = 'https://github.com/alexanderjeurissen/ranger_devicons/archive/1.0.tar.gz',
+    keywords = ['ranger', 'devicons', 'nerd-fonts', 'file-icons'],
+    classifiers = [],
+  )
+
+
+if __name__ == '__main__':
+  main()

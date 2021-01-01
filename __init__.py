d8f98ba64076b731e784f2d75b6bbad4c0e704ef
Alexander Jeurissen <alexander@jeurissen.email>
2021-01-01 17:16:48 +0100

    Fix unwanted filename mode override (#72)

--- __init__.py
+++ __init__.py
@@ -10,10 +10,3 @@ class DevIconsLinemode(LinemodeBase):
 
   def filetitle(self, file, metadata):
     return devicon(file) + ' ' + file.relative_path
-
-@ranger.api.register_linemode
-class DevIconsLinemodeFile(LinemodeBase):
-  name = "filename"
-
-  def filetitle(self, file, metadata):
-    return devicon(file) + ' ' + file.relative_path

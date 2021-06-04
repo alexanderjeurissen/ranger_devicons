958444cc4ff00d58d3cde2eaf4cfbe1dbafce6be
Florian Bruhin <me@the-compiler.org>
2021-06-04 20:40:01 +0200

    Make separator configurable
    Some terminals, such as kitty, use the space after icons to merge the two cells and display a bigger icon.
    For nicer rendering in this case, two spaces can be used as separator instead.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
+import os
 import ranger.api
 from ranger.core.linemode import LinemodeBase
 from .devicons import *
+
+SEPARATOR = os.getenv('RANGER_DEVICONS_SEPARATOR', ' ')
 
 @ranger.api.register_linemode
 class DevIconsLinemode(LinemodeBase):
@@ -9,4 +12,4 @@ class DevIconsLinemode(LinemodeBase):
   uses_metadata = False
 
   def filetitle(self, file, metadata):
-    return devicon(file) + ' ' + file.relative_path
+    return devicon(file) + SEPARATOR + file.relative_path

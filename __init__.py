9d0aba2f58f114683b684fec21a4df1aaf20a151
XuehaiPan <XuehaiPan@pku.edu.cn>
2021-09-09 19:31:34 +0800

    Patch ranger's builtin linemodes

--- __init__.py
+++ __init__.py
@@ -1,15 +1,35 @@
 import os
 import ranger.api
-from ranger.core.linemode import LinemodeBase
+from ranger.core import linemode
 from .devicons import *
 
 SEPARATOR = os.getenv('RANGER_DEVICONS_SEPARATOR', ' ')
 
+
 @ranger.api.register_linemode
-class DevIconsLinemode(LinemodeBase):
-  name = "devicons"
+class DevIconsLinemode(linemode.LinemodeBase):
+    name = "devicons"
+    uses_metadata = False
 
-  uses_metadata = False
+    def filetitle(self, file, metadata):
+        return SEPARATOR.join((devicon(file), file.relative_path))
 
-  def filetitle(self, file, metadata):
-    return devicon(file) + SEPARATOR + file.relative_path
+
+def patch_devicons(cls):
+    def filetitle(self, file, metadata):
+        return SEPARATOR.join((devicon(file),
+                               super(self.__class__, self).filetitle(file, metadata)))
+
+    return type(cls.__name__, (cls,), {'filetitle': filetitle})
+
+
+name, attr = None, None
+for name, attr in vars(linemode).items():
+    if name.endswith('Linemode'):
+        try:
+            if issubclass(attr, linemode.LinemodeBase):
+                globals()[name] = ranger.api.register_linemode(patch_devicons(attr))
+        except TypeError:
+            pass
+
+del name, attr
